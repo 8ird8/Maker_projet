7c03051aae59d9959f063711dc19593e14b7ec1e
achraf <achraf.lmoss87@hotmail.com>
2023-11-24 03:55:10 +0100

    yes

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors : {
+        'marine': "#080C20"
+      },
+      fontWeight: {
+        '100': 100,
+        '200': 200,
+        '300': 300,
+        '400' : 40,
+        '500': 500,
+        '600' : 600,
+        '700': 700,
+        '800' : 800,
+      },
+    },
+  },
+  plugins: [],
+}
+
